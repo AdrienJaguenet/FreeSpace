bb192cad4b9c43eace46cb9ecd253cabc73f48c5
Adrien Jaguenet <adrien_jaguenet@makelofine.org>
2019-10-08 17:32:40 +0200

    Three shitty functions

--- src/Scene.hpp
+++ src/Scene.hpp
@@ -9,5 +9,9 @@ class Scene
 private:
 	std::vector<Entity> ents;
 public:
+	/* returns the player's ID (position in the ents list) */
+	int SpawnPlayer();
+	Entity& GetEntity(int id);
+	void Update(int dt);
 };
 
